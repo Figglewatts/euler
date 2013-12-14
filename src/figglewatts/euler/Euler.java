3b1dc3fedfa7c15cf67e8953700bb6c9e62bb19a
Figglewatts <figglewatts@hotmail.co.uk>
2013-12-14 09:05:42 +0000

    Added Euler class which contains the main() method. Problems will be run from here.

---
+++ src/figglewatts/euler/Euler.java
@@ -0,0 +1,16 @@
+package figglewatts.euler;
+
+/**
+ * @author Figglewatts
+ */
+public class Euler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
