bea2c2c89602e3ee592ac196f670d1f52d57ee8f
Figglewatts <figglewatts@hotmail.co.uk>
2013-12-14 09:16:59 +0000

    Added correct solution to Problem 1.

--- src/figglewatts/euler/problems/oneToFifty/Problem1.java
+++ src/figglewatts/euler/problems/oneToFifty/Problem1.java
@@ -11,7 +11,26 @@ public class Problem1 extends BaseProblem {
 
 	@Override
 	public void Execute() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Beginning computation.");
+		int result = 0;
+		for (int i = 1; i < 1000; i++) {
+			if ((i % 15) == 0) {
+				// it's divisible by both 3 and 5
+				result += i; // add it to the result integer
+				continue; // skip the other checks
+			}
+			else if ((i % 5) == 0) {
+				// it's divisible by 5 only
+				result += i;
+				continue;
+			}
+			else if ((i % 3) == 0) {
+				// it's only divisible by 3
+				result += i;
+				continue;
+			}
+		}
+		System.out.println("Sum of multiples of 3 and 5 where multiple > 1 and < 1000 is equal to:");
+		System.out.println(result);
 	}
 }
