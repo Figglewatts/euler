e50929407e4f824e990a3176f95c67f3e10f154e
Figglewatts <figglewatts@hotmail.co.uk>
2013-12-14 09:06:33 +0000

    Added a class for Problem 1 which extends the BaseProblem class. Will solve in a matter of minutes.

---
+++ src/figglewatts/euler/problems/oneToFifty/Problem1.java
@@ -0,0 +1,17 @@
+package figglewatts.euler.problems.oneToFifty;
+
+import figglewatts.euler.BaseProblem;
+
+/**
+ * @author Figglewatts
+ */
+public class Problem1 extends BaseProblem {
+
+	public Problem1() { this.name = "Multiples of 3 and 5"; this.number = 1; }
+
+	@Override
+	public void Execute() {
+		// TODO Auto-generated method stub
+		
+	}
+}
