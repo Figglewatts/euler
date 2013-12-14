8b9b5ed8eb1d986b544b2c511fc641a4c983df90
Figglewatts <figglewatts@hotmail.co.uk>
2013-12-14 09:17:15 +0000

    Added functions to execute problems in Euler.java

--- src/figglewatts/euler/Euler.java
+++ src/figglewatts/euler/Euler.java
@@ -1,16 +1,27 @@
 package figglewatts.euler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import figglewatts.euler.problems.oneToFifty.Problem1;
+
 /**
  * @author Figglewatts
  */
 public class Euler {
-
-	/**
-	 * @param args
-	 */
+	private static List<BaseProblem> problemList = new ArrayList<BaseProblem>();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PopulateList();
+		SolveProblem(1);
 	}
-
+	
+	private static void PopulateList() {
+		problemList.add(new Problem1());
+	}
+	
+	public static void SolveProblem(int problemNumber) {
+		problemList.get(problemNumber-1).Execute();
+		
+	}
 }
